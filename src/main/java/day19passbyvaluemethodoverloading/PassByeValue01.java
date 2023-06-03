531b746d4104f5589903bf295c4c519d387432a0
Mesut Erdogan <mrdogan86@gmail.com>
2023-06-03 23:17:29 +0300

    JavaLessons

---
+++ src/main/java/day19passbyvaluemethodoverloading/PassByeValue01.java
@@ -0,0 +1,56 @@
+package day19passbyvaluemethodoverloading;
+
+public class PassByeValue01 {
+
+    public static void main(String[] args) {
+
+        //Note : Java "pass by value" sayesinde variable'larin orijinal degerlerini korur.
+
+        int shirtPrice = 100;
+
+        //Java methoda gonderirken orijinal degeri degil kopyasini gonderir ve degisen deger kopya deger olur.
+        System.out.println(discount("student", shirtPrice));//90
+
+        System.out.println(discount("veteran", shirtPrice));//80
+
+        System.out.println(discount("senior", shirtPrice));//95
+
+        //Normalde discount methodu ile indirim yaptik ama deger hala orijinal
+        System.out.println(shirtPrice);//100
+
+        shirtPrice = discount("veteran", shirtPrice);
+        System.out.println("shirt price kampanya fiyati: " +shirtPrice);//
+    }
+    //Discount metodunu olusturalim
+    public static int discount(String type, int price) {
+
+        switch (type) {
+            case "student":
+                price = price - 10;
+                break;
+            case "veteran":
+                price = price - 20;
+                break;
+            case "senior":
+                price = price - 5;
+                break;
+            default:
+                price = price;
+        }
+        return price;
+    }
+}
+/*
+    Pass By Value:
+    1) Java  "pass by value" kullanir
+    2) Yani; java methodlarin orjinal degeri degistirmesine musaade etmez
+    3) Java method'lara deger yollarken orjinal degerin kopyasini olusturur ve o kopyayi method'a yollar.
+        Method kopya deger üzerinde degisiklik yapar, boylece orjinal deger korunmus olur.
+    4) Java esnek bir dildir, istersek yazdigimiz kod ile orjinal degerin degismesini de temin edebiliriz.
+
+    Pass By Reference:
+    1) Pass by Reference'da method'a reference yollanir.
+    2) Reference adres demektir. Adress yollaninca method adresi kullanarak orjinal degere ulasir ve orjinal degeri degistir.
+    Bu yuzden "C#" gibi Pass By Reference kullanan dillerde method variable in orjinal degerini degistirir.
+
+ */
