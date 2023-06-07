1a689d422f03a2f47571fbf7340e1576c8f3f8b0
Mesut Erdogan <mrdogan86@gmail.com>
2023-06-07 22:33:59 +0300

    JavaLessons

---
+++ src/main/java/day22arraylistsdatetime/ArrayList03.java
@@ -0,0 +1,44 @@
+package day22arraylistsdatetime;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class ArrayList03 {
+    public static void main(String[] args) {
+
+    /*
+        Example 1: Kullanicinin girdigi harf list'te var ise o harfi "Buldum!" a cevirin, yok ise o harfi list'e ekleyin
+    */
+
+        List<String> myList = new ArrayList<>();
+
+        myList.add("A");
+        myList.add("H");
+        myList.add("J");
+
+        Scanner input = new Scanner(System.in);
+
+        int counter = 0;
+        int life = 3;
+
+        do {
+            if(counter==life){
+                System.out.println("Game is over");
+                break;
+            }
+            System.out.println("Please enter a letter");
+            String letter = input.next().toUpperCase().substring(0, 1);
+
+            if (myList.contains(letter)) {//Kullanicinin girdigi harf list'te var ise
+                myList.set(myList.indexOf(letter), "Buldum!");
+
+            } else {
+                myList.add(letter);
+            }
+            System.out.println(myList);
+
+            counter++;
+        }while (true);
+    }
+}
