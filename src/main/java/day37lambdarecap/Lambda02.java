ba19ddcf70cee948d1b23f1e540e4538a50992ef
Mesut Erdogan <mrdogan86@gmail.com>
2023-07-09 16:25:51 +0300

    JavaLessons

--- src/main/java/day37lambdarecap/Lambda02.java
+++ src/main/java/day37lambdarecap/Lambda02.java
@@ -7,44 +7,58 @@ import java.util.List;
 
 public class Lambda02 {
     public static void main(String[] args) {
-        List<String> meyve = new ArrayList<>(Arrays.asList("elma", "portakal", "uzum", "cilek", "greyfurt", ",nar",
-                "manalina", "armut", "elma", "keciboynuzu", "elma"));
 
-        ilkHarfeveyac(meyve);
-        System.out.println("\n******************");
+        List<String> meyve = new ArrayList<>(Arrays.asList("elma", "portakal", "uzum", "cilek", "greyfurt", "nar",
+                "mandalina", "armut", "elma", "keciboynuzu", "elma"));
+
+        ilkHarfeVeyac(meyve);
+        System.out.println(" \n  *******");
         basinaVeSonunaYildiz(meyve);
-        System.out.println("\n******************");
-        icindeeharfiOlanlar(meyve);
-        System.out.println("\n******************");
+        System.out.println(" \n  *******");
+        icindeeHarfiOlanlar(meyve);
+        System.out.println(" \n  *******");
         lHarfiniSil(meyve);
-        System.out.println("\n******************");
+        System.out.println(" \n  *******");
         karakterSayisinaGoreKucBuyLambdaEx(meyve);
-        System.out.println("\n******************");
+        System.out.println(" \n  *******");
+        karakterSayisinaGoreBuyKucLambdaEx(meyve);
+        System.out.println(" \n  *******");
+        karakterSayisinaGoreBuyKucMethodRef(meyve);
+
     }
 
     // SORU1 : List elemanlarının ilk harfi 'e' veya 'c' olanlari, Method References ile aralarında bosluk bırakarak yazdıralım
-    public static void ilkHarfeveyac(List<String> meyve) {
+
+    public static void ilkHarfeVeyac(List<String> meyve) {
+
         meyve.stream().filter(t -> t.startsWith("e") || t.startsWith("c")).forEach(Utils::yazdir);
 
     }
 
     // SORU2 : List elemanlarının hepsinin basina ve sonuna yildiz ekleyerek yazdiralim
+
     public static void basinaVeSonunaYildiz(List<String> meyve) {
+
         meyve.stream().map(t -> "*" + t + "*").forEach(Utils::yazdir);
 
     }
 
     // SORU3 : List elemanlarının icinde 'e' harfi olanları yazdırın
-    public static void icindeeharfiOlanlar(List<String> meyve) {
+
+    public static void icindeeHarfiOlanlar(List<String> meyve) {
+
         meyve.stream().filter(t -> t.contains("e")).forEach(Utils::yazdir);
 
     }
 
     // SORU4 : List elemanlarındaki 'l' harflerini silip yazdırınız
+
     public static void lHarfiniSil(List<String> meyve) {
+
         meyve.stream().map(t -> t.replace("l", "")).forEach(Utils::yazdir);
 
     }
+
     // SORU5 : List elemanlarini, Lambda Expression kullanarak
     // karakter sayisina göre kucukten buyuge dogru sıralayarak yazdırınız
 
--- src/main/java/day37lambdarecap/Utils.java
+++ src/main/java/day37lambdarecap/Utils.java
@@ -2,10 +2,12 @@ package day37lambdarecap;
 
 public class Utils {
     public static void yazdir(Object a) {
+
         System.out.print(a + " ");
     }
 
     public static boolean ciftPozitifBul(int a) {
+
         return a % 2 == 0 && a > 0;
     }
 }
